8004c7b934427bb0271e1c8980b0906adf18b871
lalit@lalitadithya.com <lalit@lalitadithya.com>
2021-02-03 18:46:21 +0530

    Add project files.

---
+++ AutoUpdateSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoUpdateSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
