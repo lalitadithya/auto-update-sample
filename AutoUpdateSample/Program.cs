511fb18702befcd153ca517b3a6f0b8eab5645a4
lalit@lalitadithya.com <lalit@lalitadithya.com>
2021-02-03 19:52:41 +0530

    Updated code to print current version

--- AutoUpdateSample/Program.cs
+++ AutoUpdateSample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace AutoUpdateSample
 {
@@ -8,7 +9,7 @@ namespace AutoUpdateSample
         static void Main(string[] args)
         {
             AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! You are currently running " + Assembly.GetExecutingAssembly().GetName().Version.ToString());
         }
 
         private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
