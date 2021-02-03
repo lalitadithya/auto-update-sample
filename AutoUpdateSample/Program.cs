9087a5ee4e878d16139663924f4a817d826d6af8
lalit@lalitadithya.com <lalit@lalitadithya.com>
2021-02-03 20:19:51 +0530

    Scaffold code to execute installer for update

--- AutoUpdateSample/Program.cs
+++ AutoUpdateSample/Program.cs
@@ -1,21 +1,39 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 
 namespace AutoUpdateSample
 {
     class Program
     {
+        static bool isUpdateAvailable = false;
+        static string installerWebLocation = "";
+        static string newVersion = "";
+
         static void Main(string[] args)
         {
             AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
-            Console.WriteLine("Hello World! You are currently running " + Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            string currentVersion = $"{version.Major}.{version.Minor}.{version.Revision}";
+            Console.WriteLine("Hello World! You are currently running " + currentVersion);
+
+            // check for updates
+            isUpdateAvailable = true;
+            installerWebLocation = "https://github.com";
+            newVersion = "1.1.0";
         }
 
         private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
         {
-            Console.WriteLine("Process is exiting");
-            Process.Start("notepad.exe");
+            if(isUpdateAvailable)
+            {
+                string installerLocation = Path.Join(Path.GetTempPath(), $"AutoUpdateSample.{newVersion}.msi");
+                Process.Start(new ProcessStartInfo(installerLocation)
+                {
+                    UseShellExecute = true
+                });
+            }
         }
     }
 }
